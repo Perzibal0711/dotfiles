c5d2188adc326ab6df0ed1beae96919c62b2b53d
Perzibal0711 <l23120519@morelia.tecnm.mx>
2024-11-28 18:47:53 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 # Autor:perzaival
 
@@ -8,6 +8,10 @@ sudo apt openjdk-8-sdk vim nano ... -y
 sudo apt update
 sudo apt install neofetch
 flatpak install flathub com.visualstudio.code
-wget https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXwsCne_sDKhFfgUw4s9>
+wget https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXwsCne_sDKhFfgUw4s9B2Ejw-rlK3QlP4ZQ&s
 mkdir imagenes
-mv images?q=tbn:ANd9GcRXwsCne_sDKhFfgUw4s9B2Ejw-rlK3QlP4ZQ&s imagenes
+# mv images?q=tbn:ANd9GcRXwsCne_sDKhFfgUw4s9B2Ejw-rlK3QlP4ZQ&s imagenes
+sudo apt install openjdk-11-jdk -y
+sudo apt install php -y
+sudo apt install dia -y
+flatpak install flathub org.kde.umbrello -y
