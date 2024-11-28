789d164309c797fe856ffefccd9c11766465d7a3
Perzibal0711 <l23120519@morelia.tecnm.mx>
2024-11-28 17:40:07 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+# Autor:perzaival
+
+sudo apt update && sudo apt upgrade
+sudo apt openjdk-8-sdk vim nano ... -y
+# wget https:/imagen.com/imagen.jpg
+sudo apt update
+sudo apt install neofetch
+flatpak install flathub com.visualstudio.code
+wget https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXwsCne_sDKhFfgUw4s9>
+mkdir imagenes
+mv images?q=tbn:ANd9GcRXwsCne_sDKhFfgUw4s9B2Ejw-rlK3QlP4ZQ&s imagenes
